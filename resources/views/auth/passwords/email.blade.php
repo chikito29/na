f7f9f5bff75af47f86b7d1dd693e07b2f630ef1b
Daniel Bajana <chikito029@gmail.com>
2017-04-10 12:59:56 +0800

    Added Forgot Password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,27 +1,39 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en" class="body-full-height">
+    <head>
+        <!-- META SECTION -->
+        <title>Atlant - Responsive Bootstrap Admin Template</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <link rel="icon" href="{{ url('favicon.ico') }}" type="image/x-icon" />
+        <!-- END META SECTION -->
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+        <!-- CSS INCLUDE -->
+        <link rel="stylesheet" type="text/css" id="theme" href="{{ url('css/theme-default.css') }}"/>
+        <!-- EOF CSS INCLUDE -->
+    </head>
+    <body>
+
+        @if (session('status'))
+            <div class="error-container">
+                <div class="error-text">Password Reset Link Sent!</div>
+                <div class="error-subtext">{{ session('status') }}</div>
+            </div>
+        @else
+            <div class="registration-container">
+                <div class="registration-box animated fadeInDown">
+                    <div class="registration-logo"></div>
+                    <div class="registration-body">
+                        <div class="registration-title"><strong>Forgot</strong> Password?</div>
+                        <div class="registration-subtitle">Enter your email. We'll email instructions on how to reset your password.</div>
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
-
+                        <h4>Your E-mail</h4>
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="example@example.com" required>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -29,18 +41,28 @@
                                 @endif
                             </div>
                         </div>
+                        <div class="form-group push-up-20">
+                            <div class="col-md-6">
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
+                            </div>
+                            <div class="col-md-6">
+                                <button class="btn btn-danger btn-block">Send Reset Link</button>
                             </div>
                         </div>
-                    </form>
+                        </form>
+                    </div>
+                    <div class="registration-footer">
+                        <div class="pull-left">
+                            &copy; 2017 NEWSIM ACCOUNTS
+                        </div>
+                        <div class="pull-right">
+                            <a href="#">About</a> |
+                            <a href="#">Privacy</a> |
+                            <a href="#">Contact Us</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+        @endif
+    </body>
+</html>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,44 +1,54 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en" class="body-full-height">
+    <head>
+        <!-- META SECTION -->
+        <title>Atlant - Responsive Bootstrap Admin Template</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <!-- END META SECTION -->
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <!-- CSS INCLUDE -->
+        <link rel="stylesheet" type="text/css" id="theme" href="/css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->
+    </head>
+    <body>
 
+        <div class="login-container login-v2">
+
+            <div class="login-box animated fadeInDown">
+                <div class="login-body">
+                    <div class="login-title"><strong>Reset </strong> Password</div>
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('password.request') }}">
                         {{ csrf_field() }}
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <span class="fa fa-user"></span>
+                                    </div>
+                                    <input id="email" type="email" class="form-control" placeholder="Email" name="email" value="{{ $email or old('email') }}" required autofocus>
+                                </div>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong class="text-danger">{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <span class="fa fa-lock"></span>
+                                    </div>
+                                    <input id="password" placeholder="Password" type="password" class="form-control" name="password" required>
+                                </div>
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -46,12 +56,14 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
+                            <div class="col-md-12">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <span class="fa fa-lock"></span>
+                                    </div>
+                                    <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -59,18 +71,25 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reset Password
-                                </button>
+                            <div class="col-md-12">
+                                <button class="btn btn-primary btn-lg btn-block">Reset Password</button>
                             </div>
                         </div>
                     </form>
                 </div>
+                <div class="login-footer">
+                    <div class="pull-left">
+                        &copy; 2017 NEWSIM ACCOUNTS
+                    </div>
+                    <div class="pull-right">
+                        <a href="#">About</a> |
+                        <a href="#">Privacy</a> |
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
             </div>
+
         </div>
-    </div>
-</div>
-@endsection
+    </body>
+</html>
